7d7bf1e516562bc89188c3c0420e161031e818fa
hamzaazeem.tbox@gmail.com <grubsterscomicx@ns1004370.ip-92-204-133.us>
2020-11-24 06:49:39 +0000

    server configuration

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'grubster_wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'grubster_admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'G619F4e04@' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
